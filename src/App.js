74267cabbeafda6895e0ea2ef0f0c88c4346a1c9
Yameen-Baloch <yaminbaloch94@gmail.com>
2022-06-13 12:11:33 +0500

    initail Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 
+import SearchBox from './components/search-box/SearchBox';
+import CardList from './components/card-list/CardList';
+
 function App() {
+
+  // Call Use State for Handle Search
+  const [searchFeild, setSearchField] = useState('');
+  
+  // Call useState for calling Monsters
+  const [monster, setMonsters] = useState([]);
+
+  // call useEffect for api call
+  useEffect(() =>{
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => setMonsters(users))
+  }, [])
+
+  
+
+  // HandleSearch method for OnchangeHandler
+  const handleSearch = (e) => {
+    const searchText = e.target.value.toLocaleLowerCase();
+    setSearchField(searchText);
+  }
+
+  // Function for filtering monsters
+
+  const filteredMonsters = monster.filter((monster) =>{
+    return monster.name.toLocaleLowerCase().includes(searchFeild);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+
+      {/* Here is the searchBox component Called */}
+
+      <SearchBox 
+      className='search-box'
+      onChangeHandler={handleSearch}
+      placeholder='Search Mosnter' />
+
+      {/* Call the Card List Component Here */}
+      <CardList  monster={filteredMonsters}/>
+      
     </div>
   );
 }
---
+++ src/components/search-box/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SearchBox.css'
+
+const SearchBox = (props) => {
+  return (
+    <input
+    className={props.className}
+    type='search'
+    placeholder={props.placeholder}
+    onChange={props.onChangeHandler} />
+  )
+}
+
+export default SearchBox
